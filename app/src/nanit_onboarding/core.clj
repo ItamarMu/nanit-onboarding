4cebf349f10992fad67d0c7f1029680be16e3f78
itamarmutzafi <freg10@gmail.com>
2022-05-29 12:23:08 +0300

    some cleaning

--- app/src/nanit_onboarding/core.clj
+++ app/src/nanit_onboarding/core.clj
@@ -3,13 +3,6 @@
       [nanit-onboarding.web.router :refer [all-routes]])
     (:gen-class))
 
-(defn app [req]
-      {:status  200
-       :headers {"Content-Type" "text/html"}
-       :body    "hello HTTP!"})
-
-;(run-server app {:port 8080})
-
 (defn -main
       [& args]
       (run-server all-routes {:port 8080}))
