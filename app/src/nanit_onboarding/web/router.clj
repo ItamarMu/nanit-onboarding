1ff9dc4582b5f1d9371ea5bcd63a41342dc9e1a7
itamarmutzafi <freg10@gmail.com>
2022-05-31 17:03:10 +0300

    added create route with tests and docker files

--- app/src/nanit_onboarding/web/router.clj
+++ app/src/nanit_onboarding/web/router.clj
@@ -1,5 +1,29 @@
 (ns nanit-onboarding.web.router
-    (:require [compojure.core :refer [defroutes GET POST DELETE ANY context]]))
+    (:require [compojure.core :refer [defroutes GET POST DELETE ANY context]]
+              [nanit-onboarding.models.post :as post]
+      [ring.middleware
+       [json :refer [wrap-json-params wrap-json-response]]
+       [content-type :refer [wrap-content-type]]
+       [keyword-params :refer [wrap-keyword-params]]
+       [params :refer [wrap-params]]
+       ]))
 
 (defroutes all-routes
-           (GET "/ping" [] {:body "pong"}))
+           (GET "/ping" []
+                {:status 200
+                 :headers {"Content-Type" "text/html"}
+                 :body "pong"})
+           (POST "/post" req
+             {:status  200
+              :headers {"Content-Type" "text/html"}
+              :body    (post/create (:params req))}
+             ))
+
+(def app
+  (-> all-routes
+      wrap-content-type
+      wrap-keyword-params
+      wrap-params
+      wrap-json-params
+      wrap-json-response))
+
